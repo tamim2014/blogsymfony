6d410fdf49a3334385d9fa6eee0439eb7f55856e
unknown <a.andjib@yahoo.fr>
2019-09-17 20:26:37 +0200

    ArticleType.php: Mise a jour du formulaire Add/Edit et des templates correspondants(pour l'integration de la category d1 article)

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
@@ -12,11 +14,10 @@ class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('title')
-            ->add('content')
-            ->add('image')
-        ;
+        $builder->add('title')
+                ->add('category', EntityType::class, [ 'class' => Category::class, 'choice_label' => 'title' ])                        
+                ->add('content')
+                ->add('image');       
     }
 
     public function configureOptions(OptionsResolver $resolver)
