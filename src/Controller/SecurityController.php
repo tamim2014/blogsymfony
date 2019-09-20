59c4aab27199fafa93eda63aa1e01c3fa290bf57
unknown <a.andjib@yahoo.fr>
2019-09-20 16:50:55 +0200

    Entite User, Formulaire RegistrationType, Controleur SecurityController et son template

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("inscription", name = "security_registration")
+     */
+    public function registration(Request $request, ObjectManager $manager){
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+        }
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+ 
+}
