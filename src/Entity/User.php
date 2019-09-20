5b1051575611d6b37b0ef0336e6a1f06830b3af7
unknown <a.andjib@yahoo.fr>
2019-09-20 17:49:10 +0200

    algorithm bcrypt: pour crypter les mot de passe lors de l'inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface 
 {
     /**
      * @ORM\Id()
@@ -33,7 +35,6 @@ class User
      */
     private $password;
     /**
-     * @ORM\Column(type="string", length=255)
      * @Assert\Length(min="4",  minMessage="Votre mot de passe de doit faire 4 caractere au minimum")
      * @Assert\EqualTo(propertyPath="password" , message="Ce n'est pas le mem mot de passe")
      */
@@ -79,4 +80,13 @@ class User
 
         return $this;
     }
+
+    // les fonctions de l'interface UserInterface (On l'utilise pour indiquer a symfony que User est bien une classe utilisateur afin de pouvoir crypter les mot de passe lors d'une inscription)
+    public function eraseCredentials(){}
+    public function getSalt(){}
+    public function getRoles() {
+        // Quel est le rou de cet utilisateur (administrateur? ou simple utilisateur?)
+        return ['ROLE_USER'];
+    }
+
 }
