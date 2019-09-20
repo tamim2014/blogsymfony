037a1f27b9ba4ea3a7ee48e25505371007741bf5
unknown <a.andjib@yahoo.fr>
2019-09-20 20:35:25 +0200

    security.yaml: l'authentification se passe ICI. Definition du provider in_databse et du chemin pour se loger(login_path:...)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,10 +26,17 @@ class SecurityController extends AbstractController
 
             $manager->persist($user);
             $manager->flush();
+            return $this->redirectToRoute('security_login');
         }
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(){
+        return $this->render('security/login.html.twig');
     }
  
 }
